f2bb2fe13be04b548ddc60494416b91e9cc2c9a9
Dimkajasons <dimkajasons@gmail.com>
2018-05-07 15:57:21 +0300

    solved tusks

--- arrays.js
+++ arrays.js
@@ -9,19 +9,63 @@ var actors = [
 ]
 
 //1. Получить массив который будет содержать всех актеров родившихся до 1950го года
+function getOldActors (arr) {
+    return arr.filter((element) => element.year < 1950)
+}
+console.log(getOldActors(actors))
+
 
 //2. Получить массив который будет содержать полное имя каждого актера
+function getFullName(arr) {
+    return arr.map((element) => element.firstName + ' ' + element.lastName)
+}
+console.log(getFullName(actors))
+
 
 //3. Отсортировать массив по возрасту актеров - от старшего
+function filterByAge(arr) {
+    return arr.sort((a, b) => a.year - b.year)
+}
+console.log(filterByAge(actors));
+
 
 //4. Какой общий возраст всех актеров (предположим что ворзраст актера равен 2018-actor.year)
+function getTotalAge(arr) {
+    return arr.reduce(function(prev, curr) {
+        return prev + 2018-curr.year
+    }, 0)
+}
+console.log(getTotalAge(actors));
+
 
 var people = ['John, Lock', 'Colin, Hull', 'Jane, Fox', 'Nick, Milner', 'Sarah, Connor', 'David, Smith', 'Hannah, Rose',
 'Peter, Todd', 'Frank, Sample', 'Luke, Pope', 'Alissa, Moss', 'Sam, Washington', 'Eddie, Cope', 'Lucy, Bower', 'Andre, Cox',
 'Monica, Bell', 'Richard, Nowell', 'Chris, Paxton', 'John, Cho', 'Tom, Price', 'Kate, Ritter', 'Mike, Lee', 'Kristin, Spawn',]
 
 //5. Отсортировать в алфовитном порядке по фамилии
+function sortByLastName(arr) {
+    let changedArr = arr.map((elem) => elem.split(', ').reverse().join(' '));
+    return changedArr.sort()
+}
+console.log(sortByLastName(people))
+
 
 
 var data = ['apple', 'banana', 'apple', 'orange', 'banana', 'orange', 'apple', 'lemon', 'pear', 'pear', 'banana']
 //6*. посчитать количество каждого фрукта в массиве - вывести объект например - fruitsCount = {apple: 4, banana: 5, ...}
+function counter(arr) {
+    let result = {};
+    arr.forEach(function(element) {
+        !result[element] ? result[element] = 1 : result[element] += 1;
+    });
+    return result;
+}
+console.log(counter(data));
+
+// {
+//     if (!result[element]) {
+//         result[element] = 1;
+//     } else {
+//         result[element] += 1;
+//     }
+// }
